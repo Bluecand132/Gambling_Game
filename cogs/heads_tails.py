fc3383a67da63c3a31077f9ed89c127f209d3dfb
Bluecand132 <teodorhobjila@yahoo.com>
2024-03-08 10:54:43 +0000

    made the loop work for heads_tails.py
    it should now be able to loop the input validation until either one of the choices are chosen.

--- cogs/heads_tails.py
+++ cogs/heads_tails.py
@@ -1,19 +1,42 @@
+#make sure that the time is spaces (appropriate time between each print statement)
 import time
 import random
 array = ['heads' , 'tails']
 #score = from database
 
+#input validation
+def input_validation():
+    loop = True
+    while loop == True:
+        choice_user = str(input("Choose: \n*Heads \n*Talis \nPut answer here: "))
+        choice_user = choice_user.lower()
+        if choice_user in array:
+            time.sleep(1.5)
+            print(f"Ok, you have chosen: {choice_user}")
+            loop = False
+            heads_tails_game(choice_user)
+
+        else:
+            print("Please look at the options, and try again.")
+            time.sleep(1)
+
+
+
 #game chooses heads or tails
-def heads_tails_game():
+def heads_tails_game(choice_user):
     place_holder = random.randint(0, 1)
     choice_game = array[place_holder]
 
+    print("The results are in!")
+    time.sleep(1.5)
+    print("And you are...")
+
     #choices
     if choice_game == choice_user:
-        print("Good job! You got it right. Heres 'x' amount of points.")
+        print("Lucky! You got it right. Heres 'x' amount of points.")
         #decrease score from database
     else:
-        print("Unlucky, you lost 'x' amount of points. Better luck next time.")
+        print("Unlucky. You lost 'x' amount of points. Better luck next time.")
         #decrease score from database
 
 
@@ -22,36 +45,4 @@ def heads_tails_game():
 #introduction
 print("Welcome to heads or tails.")
 time.sleep(1.5)
-
-#user chooses heads or tails
-while True:
-    try:
-        choice_user = str(input("Choose: \n*Heads \n*Talis \nPut answer here: "))
-        if choice_user in array:
-            print(f"Ok, you have chosen: {choice_user}")
-            loop = False
-        else:
-            print("Please look at the options.")
-
-    except:
-        print("Please look at the options.")
-
-
-'''    except choice_user not in array:
-        print("Please look at the options.")
-        continue
-    else:
-        choice_user = choice_user.lower()
-        print(f"Ok, you have chosen: {choice_user}.")
-        break
-'''
-
-'''#input validation
-if choice_user in array:
-    print(f"Ok, you have chosen: {choice_user}.")
-    #proceeds with the game
-    heads_tails_game()
-else:
-    print("Please input a valid choice.")
-    #loop it back to choice_user
-'''
+input_validation()
